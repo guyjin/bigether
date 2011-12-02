090a8481d170a456d4274ca1d6baaeb8849d1806
Ben.Vaughan <benjamin.vaughan@gmail.com>
2011-12-01 22:17:06 -0700

    swapped out old files for new versions.
    deleted the old files.  Added new versions from html5 boilerplate
    including the build scripts.  This has some preliminary design  and
    markup for a new layout.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,36 @@
-/* Author:
-
+/* Author: 
+	Ben Vaughan
 */
+jQuery(document).ready(function() {
+	var a = $('nav a').length;
+	$("nav a").each(function (i) {
+	    // store the item around for use in the 'timeout' function
+	    var $item = $(this); 
+	    // execute this function sometime later:
+	    setTimeout(function() { 
+	      $item.animate({"marginTop": 0}, 1500,'easeOutQuint');
+	    }, 100*i);
+	    // each element should animate half a second after the last one.
+	  });
+	
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
